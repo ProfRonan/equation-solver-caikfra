0c3a8573aa478e33ff1d0a3e107230d116cf4579
Caik Franco <caikfra@alu.ufc.br>
2023-05-15 23:26:07 -0300

    Arquivo main.py atualizado

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ grau = int(input('Digite o grau da equação: '))
 if grau < 1 or grau > 2:
     print("Grau inválido")
 
-# grau 1
+#GRAU 1
 else:
     if grau == 1:
         print("A equação é do primeiro grau")
@@ -16,26 +16,26 @@ else:
             b = int(input("Digite o valor de b: "))
             x = -b / a
             print(f"{x:.2f}")
-        
+#GRAU 2
     if grau == 2:
         print("A equação é do segundo grau")
         a = int(input("Digite o valor de a: "))
         if a == 0:
             print('Valor de a inválido')
-        else:
+        elif a !=0:
             b = int(input("Digite o valor de b: "))
             c = int(input("Digite o valor de c: "))
-            delta = (b**2)-(4*a*c)
+            delta = b**2-4*a*c
             if delta < 0:
                 print('A equação não possui raízes reais')
             elif delta == 0:
-                print("A equação possui apenas uma raiz real")
                 print('A equação possui uma raiz real')
-                x = (-b+delta**0.5)/2*a
+                x = ((-b)+(delta**0.5))/2*a
                 print(f'{x:.2f}')
             elif delta > 0:
-                x1 = (-b - delta**0.5)/2*a
-                x2 = (-b + delta**0.5)/2*a
-                print("A equação possui duas raízes reais")
+                print('A equação possui duas raízes reais')
+                raiz_de_delta = delta**0.5 
+                x1 = (((-b)-raiz_de_delta))/(2*a)
+                x2 = (((-b)+raiz_de_delta))/(2*a)
                 print(f"{x1:.2f}")
                 print(f'{x2:.2f}')
